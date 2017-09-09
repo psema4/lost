6f3e52a685554ff4da76752db2f0b3769f887bde
Scott Elcomb <psema4@gmail.com>
2017-09-08 23:11:39 -0400

    move globals to utils

--- src/engine/engine.js
+++ src/engine/engine.js
@@ -1,21 +1,3 @@
-//TODO: World map: put doors on outer walls (solve 2-way travel)
-
-DEBUG = true;
-
-LYR_FLOORS = 0;
-LYR_WALLS = 1;
-LYR_DOORS = 2;
-LYR_PICKUPS = 3;
-LYR_ACTORS = 4;
-
-// alias Z to W, Q to A for AZERTY keyboards; see http://xem.github.io/articles/#jsgamesinputs
-KEY_W = 87; KEY_Z = 90; KEY_UP = 38;
-KEY_A = 65; KEY_Q = 81; KEY_LEFT = 37;
-KEY_S = 83; KEY_DOWN = 40;
-KEY_D = 68; KEY_RIGHT =  39;
-KEY_SPACE = 32;
-KEY_ENTER = 13;
-
 function Engine(opts) {
     opts = opts || {};
     var sprite;
--- src/util.js
+++ src/util.js
@@ -1,2 +1,18 @@
+DEBUG = true;
+
+LYR_FLOORS = 0;
+LYR_WALLS = 1;
+LYR_DOORS = 2;
+LYR_PICKUPS = 3;
+LYR_ACTORS = 4;
+
+// alias Z to W, Q to A for AZERTY keyboards; see http://xem.github.io/articles/#jsgamesinputs
+KEY_W = 87; KEY_Z = 90; KEY_UP = 38;
+KEY_A = 65; KEY_Q = 81; KEY_LEFT = 37;
+KEY_S = 83; KEY_DOWN = 40;
+KEY_D = 68; KEY_RIGHT =  39;
+KEY_SPACE = 32;
+KEY_ENTER = 13;
+
 function _$(sel) { return document.querySelector(sel); }
 function _$$(sel) { return document.querySelectorAll(sel); }
